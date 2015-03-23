f6ae6d793f484a69e6aa0e8a15b26f4e72821432
rodneyxr <rodneyxr@gmail.com>
2015-03-22 19:19:32 -0500

    added TDTK Free assets

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public Transform target;
+    private NavMeshAgent nav;
+
+    void Start() {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void Update() {
+        nav.SetDestination(target.position);
+    }
+}
