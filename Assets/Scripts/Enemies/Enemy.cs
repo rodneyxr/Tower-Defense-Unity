84d0ef41880a9da3020e70ea26e1c6942cc3578b
rodneyxr <rodneyxr@gmail.com>
2015-04-09 16:54:28 -0500

    added waypoints

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -3,9 +3,15 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
+    public Transform target;
+    public float speed = .5f;
+
+
     void Start() {
     }
 
     void Update() {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * speed);
     }
+
 }
---
+++ Assets/Scripts/WayPoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WayPoint : MonoBehaviour {
+
+    public WayPoint next;
+    public bool start;
+    public bool end;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
